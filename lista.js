aa798a62b36abeafc56b99a105ebd6ebba4c0d9b
joaofranka2 <126894788+joaofranka2@users.noreply.github.com>
2023-08-11 01:46:45 -0300

    inicio

---
+++ lista.js
@@ -0,0 +1,11 @@
+class Tarefa {
+    constructor(descricao, autor, departamento, importancia) {
+        this.descricao = descricao;
+        this.autor = autor;
+        this.departamento = departamento;
+        this.importancia = importancia;
+        this.valor = null;
+        this.duracao = null;
+    }
+}
+
