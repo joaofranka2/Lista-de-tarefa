68034fde95879cdafffbdcbd395f49b3334cbb82
joaofranka2 <126894788+joaofranka2@users.noreply.github.com>
2023-08-11 01:56:50 -0300

    atualizaçao

--- lista.js
+++ lista.js
@@ -9,3 +9,30 @@ class Tarefa {
     }
 }
 
+class ListaDeTarefas {
+    constructor() {
+        this.tarefas = [];
+    }
+
+    incluirTarefa(tarefa) {
+        this.tarefas.push(tarefa);
+    }
+
+    excluirTarefaConcluida(indice) {
+        this.tarefas.splice(indice, 1);
+    }
+
+    adicionarDetalhe(indice, detalhe, valor) {
+        this.tarefas[indice][detalhe] = valor;
+    }
+
+    listarTarefasPorImportancia() {
+        return this.tarefas
+            .slice()
+            .sort((a, b) => b.importancia - a.importancia)
+            .map(tarefa => tarefa.descricao);
+    }
+}
+
+// Exemplo de uso
+const lista = new ListaDeTarefas();
